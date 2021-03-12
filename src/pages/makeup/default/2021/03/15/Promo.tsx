25f2deba9395856e24172f45fb0f99e236cce760
Dave Welsh <dave@qccareerschool.com>
2021-03-12 16:09:13 -0500

    Add makeup promos for rest of March

--- src/pages/makeup/default/2021/03/15/Promo.tsx
+++ src/pages/makeup/default/2021/03/15/Promo.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
-import { usePopup } from '../../../../../hooks/usePopup';
-import { useScreenWidthContext } from '../../../../../hooks/useScreenWidthContext';
+import { usePopup } from '../../../../../../hooks/usePopup';
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
 
 type Props = {
   date: Date;
   currencyCode: string;
 }
 
-export const Promo20210302: React.FC<Props> = ({ date, currencyCode }) => {
+export const Promo20210315: React.FC<Props> = ({ date, currencyCode }) => {
   const [ popup, togglePopup ] = usePopup(false);
   const screenWidth = useScreenWidthContext();
 
@@ -19,7 +19,7 @@ export const Promo20210302: React.FC<Props> = ({ date, currencyCode }) => {
   let width: number;
   let height: number;
 
-  if (date >= new Date('2021-03-10T12:00:00-05:00')) {
+  if (date >= new Date('2021-03-24T12:00:00-04:00')) {
     if (desktop) {
       image = currencyCode === 'GBP' ? require('./desktop-ends-uk.jpg') : require('./desktop-ends.jpg');
     } else {
@@ -35,14 +35,14 @@ export const Promo20210302: React.FC<Props> = ({ date, currencyCode }) => {
 
   if (desktop) {
     width = 976;
-    height = 562;
+    height = 529;
   } else {
     width = 600;
     height = 746;
   }
 
   return (
-    <section id="promoSection" style={{ backgroundColor: '#6205b0', padding: 0 }}>
+    <section id="promoSection" style={{ backgroundColor: '#badac1', padding: 0 }}>
       <div className="container px-0">
         <div className="text-center">
           <button className="btn btn-link p-0 border-0 btn-no-hover-shadow" onClick={togglePopup}><img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" /></button>
@@ -51,7 +51,7 @@ export const Promo20210302: React.FC<Props> = ({ date, currencyCode }) => {
       <Modal size="lg" isOpen={popup} toggle={togglePopup}>
         <ModalHeader toggle={togglePopup}>Special Offer</ModalHeader>
         <ModalBody className="text-center p-0">
-          <p className="mt-3">Get started today with our <strong>lowest deposit ever</strong>, followed by low monthly payments. Plus, receive a free Elite Makeup Kit when you enroll in the <strong>Master Makeup Artistry Course</strong>.</p>
+          <p className="mt-3">Get started today with our <strong>lowest deposit ever</strong>, followed by low monthly payments. Plus, enroll in the <strong>Master Makeup Artistry Course</strong> and receive the <strong>Pro Makeup Workshop</strong> and a makeup kit for free.</p>
           <img className="img-fluid" src={require('./popup-makeup-kit.jpg')} alt="Makeup Kit" />
         </ModalBody>
         <ModalFooter>
--- src/pages/makeup/default/DefaultPromo.tsx
+++ src/pages/makeup/default/DefaultPromo.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import { Promo20210302 } from './2021/03/Promo';
-import { Promo20210201 } from './2021/02/Promo';
+import { Promo20210313 } from './2021/03/13/Promo';
+import { Promo20210302 } from './2021/03/02/Promo';
+import { Promo20210315 } from './2021/03/15/Promo';
 
 type Props = {
   date: Date;
   currencyCode: string;
 }
 
-export const DefaultPromo: React.FC<Props> = ({ date, currencyCode }) => date >= new Date('2021-03-02T08:00:00-05:00')
-  ? <Promo20210302 date={date} currencyCode={currencyCode} />
-  : <Promo20210201 date={date} currencyCode={currencyCode} />;
+export const DefaultPromo: React.FC<Props> = ({ date, currencyCode }) =>
+  date >= new Date('2021-03-15T09:00:00-04:00')
+    ? <Promo20210315 date={date} currencyCode={currencyCode} />
+    : date >= new Date('2021-03-13T08:00:00-05:00')
+      ? <Promo20210313 date={date} />
+      : <Promo20210302 date={date} currencyCode={currencyCode} />;
