acc43e2e817b0760ed70ed3f31a24c03f18c7a6e
Dave Welsh <dave@qccareerschool.com>
2021-03-29 13:30:12 -0400

    Update other makeup carts

--- src/pages/makeup/limited-time-offer/index.tsx
+++ src/pages/makeup/limited-time-offer/index.tsx
@@ -17,6 +17,7 @@ const LimitedTimeOffer: React.FC = () => (
       agreementLink="https://www.qcmakeupacademy.com/enrollment-agreement.html"
       agreementLinkGB="https://www.qcmakeupacademy.com/enrollment-agreement-gb.html"
       successLink="https://www.qcmakeupacademy.com/welcome-to-the-school/"
+      showDynamicCourseDescriptions={true}
       additionalOptions={additionalOptions}
     />
   </>
