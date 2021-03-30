642054e75c345336234fb761a346f8c6cf9ec8b1
Dave Welsh <dave@qccareerschool.com>
2021-03-30 11:17:25 -0400

    Debug date comparison

--- src/pages/makeup/default/DefaultPromo.tsx
+++ src/pages/makeup/default/DefaultPromo.tsx
@@ -11,7 +11,13 @@ type Props = {
 
 export const DefaultPromo: React.FC<Props> = ({ date, currencyCode }) => {
   alert(date);
+  alert(typeof date);
+  alert(new Date('2021-03-29T09:09:00-0400'));
+  alert(typeof new Date('2021-03-29T09:09:00-0400'));
   alert(date >= new Date('2021-03-29T09:09:00-0400'));
+  alert(date > new Date('2021-03-29T09:09:00-0400'));
+  alert(date < new Date('2021-03-29T09:09:00-0400'));
+  alert(date <= new Date('2021-03-29T09:09:00-0400'));
 
   if (date >= new Date('2021-03-29T09:09:00-0400')) {
     return <Promo20210329 date={date} currencyCode={currencyCode} />;
