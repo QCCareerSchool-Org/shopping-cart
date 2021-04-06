3b11ed4df823969f2732c0544466c8466876c56c
Dave Welsh <dave@qccareerschool.com>
2021-04-06 09:50:22 -0400

    Change event student cart discount to flat 50

--- src/pages/event/student/index.tsx
+++ src/pages/event/student/index.tsx
@@ -7,7 +7,12 @@ import { courseGroups } from '../courseGroups';
 import { Guarantee } from '../Guarantee';
 
 const additionalOptionsNone = {};
-const additionalOptionsStudentDiscount = { studentDiscount: true };
+const additionalOptions50Discount = {
+  discount: {
+    default: 50,
+  },
+  discountSignature: 'IMAuLJd5/CAdMYA8xwD03ca3dSGR+Yi24+9QK5Rn6O44BwkPAojr6IMZMmFVt0yC24iI0N37R4gJj8+N9XTS1aywyFRYwVH4nNcil11xPNNhIygCkjXyGd+uE09LsLnYpEwt8Q0eS6LieMsyr7tDjUztKdI5AWd5dLNobMkAptrDeCALzvNbtg5NiGrD6Fv844Q4EEuXk8WOTpjqLqI86UKn4JECgGSntN8fwQXCEU0GqWXXxwUaWytDkMGi3KE+Ffm9fLx/ZVQSUHFWVbgcxlU0r2LVBVKeoBHnsrVxVEh6FZOfdHfj/SE1l3fs1ZsD9XDHH6EJGO0LVKW6VuBISA==',
+};
 
 const Student: React.FC = () => {
   const serverDate = useDateContext();
@@ -23,7 +28,7 @@ const Student: React.FC = () => {
       agreementLinkGB="https://www.qceventplanning.com/enrollment-agreement-gb.html"
       successLink="https://www.qceventplanning.com/welcome-to-the-school/"
       student={true}
-      additionalOptions={date.getTime() >= Date.UTC(2021, 3, 6, 13) ? additionalOptionsStudentDiscount : additionalOptionsNone}
+      additionalOptions={date.getTime() >= Date.UTC(2021, 3, 6, 13) ? additionalOptions50Discount : additionalOptionsNone}
     />
   );
 };
