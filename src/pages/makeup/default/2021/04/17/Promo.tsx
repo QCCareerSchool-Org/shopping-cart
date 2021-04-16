1a4354ac1e3cd5ec6fb87ce709b746b011202338
Dave Welsh <dave@qccareerschool.com>
2021-04-16 15:01:11 -0400

    Add makeup promos

--- src/pages/makeup/default/2021/04/17/Promo.tsx
+++ src/pages/makeup/default/2021/04/17/Promo.tsx
@@ -5,17 +5,15 @@ import { usePreloadImage } from '../../../../../../hooks/usePreloadImage';
 import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
 import { useStateContext } from '../../../../../../hooks/useStateContext';
 
-import couponButtonSrc from './coupon-btn.svg';
-import couponButtonAppliedSrc from './coupon-btn-applied.svg';
-import { url } from 'inspector';
-import { PriceResult } from '../../../../../../state/price';
+import couponButtonSrc from './coupon-btn-elite.svg';
+import couponButtonAppliedSrc from './coupon-btn-elite-active.svg';
 
 type Props = {
   date: Date;
   currencyCode: string;
 }
 
-export const Promo20210417: React.FC<Props> = ({ date, currencyCode }) => {
+export const Promo20210417: React.FC<Props> = ({ date }) => {
   const screenWidth = useScreenWidthContext();
   const { price } = useStateContext();
   const dispatch = useDispatchContext();
@@ -28,60 +26,50 @@ export const Promo20210417: React.FC<Props> = ({ date, currencyCode }) => {
   let width: number;
   let height: number;
 
-  if (date.getTime() >= Date.UTC(2021, 3, 14, 16)) {
+  if (date.getTime() >= Date.UTC(2021, 3, 18, 4)) { // April 18 00:00
     if (desktop) {
-      image = currencyCode === 'GBP' ? require('./desktop-ends-uk.jpg') : require('./desktop-ends.jpg');
+      image = require('./desktop-ends.jpg');
     } else {
-      image = currencyCode === 'GBP' ? require('./mobile-ends-uk.jpg') : require('./mobile-ends.jpg');
+      image = require('./mobile-ends.jpg');
     }
   } else {
     if (desktop) {
-      image = currencyCode === 'GBP' ? require('./desktop-uk.jpg') : require('./desktop.jpg');
+      image = require('./desktop.jpg');
     } else {
-      image = currencyCode === 'GBP' ? require('./mobile-uk.jpg') : require('./mobile.jpg');
+      image = require('./mobile.jpg');
     }
   }
 
   if (desktop) {
     width = 976;
-    height = 418;
+    height = 563;
   } else {
     width = 600;
-    height = 403;
+    height = 732;
   }
 
-  const button1Click = () => {
-    dispatch({ type: 'ADD_COURSE', payload: { courseCode: 'MZ', internal: false } });
-    dispatch({ type: 'SET_PROMO_CODE', payload: 'SPRING21' });
-  };
-
-  const button2Click = () => {
-    dispatch({ type: 'ADD_COURSE', payload: { courseCode: 'MZ', internal: false } });
-    dispatch({ type: 'SET_PROMO_CODE', payload: 'SPRING21' });
-  };
-
-  const button3Click = () => {
-    dispatch({ type: 'ADD_COURSE', payload: { courseCode: 'MZ', internal: false } });
-    dispatch({ type: 'SET_PROMO_CODE', payload: 'SPRING21' });
+  const buttonClick = () => {
+    // dispatch({ type: 'ADD_COURSE', payload: { courseCode: 'MZ', internal: false } });
+    dispatch({ type: 'SET_PROMO_CODE', payload: 'ELITE' });
   };
 
   return (
     <>
-      <section id="promoSection" style={{ backgroundColor: '#dff1ff', padding: 0 }}>
-        <div className="container text-center">
-          <div className="row">
-            <div className="col-12 col-md-4">
-              <div style={{ backgroundSize: 'cover', backgroundImage: `url(${require('./tag1.jpg')})` }}>
-                <p>sldkfjdslkfjsdflkjsdf</p>
-                {price?.promoCode === 'SPRING21'
-                  ? <img src={couponButtonAppliedSrc} width="399" height="62" className="img-fluid" alt="Promo Code" />
-                  : <button onClick={button1Click} className="btn btn-link p-0 border-0 btn-no-hover-shadow"><img src={couponButtonSrc} width="399" height="62" className="img-fluid" alt="Promo Code" /></button>
-                }
-              </div>
-            </div>
+      <section id="promoSection" style={{ backgroundColor: '#000', padding: 0 }}>
+        <div className="container px-0">
+          <div className="text-center">
+            <img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" />
           </div>
         </div>
       </section>
+      <div className="text-white" style={{ backgroundColor: '#000' }}>
+        <div className="container pt-4 pb-2 d-flex justify-content-center">
+          {price?.promoCode === 'ELITE'
+            ? <img src={couponButtonAppliedSrc} width="399" height="62" className="img-fluid" alt="Promo Code" />
+            : <button onClick={buttonClick} className="btn btn-link p-0 border-0 btn-no-hover-shadow"><img src={couponButtonSrc} width="399" height="62" className="img-fluid" alt="Promo Code" /></button>
+          }
+        </div>
+      </div>
     </>
   );
 };
