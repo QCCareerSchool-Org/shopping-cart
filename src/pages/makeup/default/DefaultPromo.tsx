0f3fe8a02756b92c2f272dfff47cb829d049ec78
Dave Welsh <dave@qccareerschool.com>
2021-04-29 13:43:20 -0400

    Add Makeup promos for may

--- src/pages/makeup/default/DefaultPromo.tsx
+++ src/pages/makeup/default/DefaultPromo.tsx
@@ -6,6 +6,8 @@ import { Promo20210329 } from './2021/03/29/Promo';
 import { Promo20210406 } from './2021/04/06/Promo';
 import { Promo20210417 } from './2021/04/17/Promo';
 import { Promo20210419 } from './2021/04/19/Promo';
+import { Promo20210501 } from './2021/05/01/Promo';
+import { Promo20210503 } from './2021/05/03/Promo';
 
 type Props = {
   date: Date;
@@ -13,7 +15,11 @@ type Props = {
 }
 
 export const DefaultPromo: React.FC<Props> = ({ date, currencyCode }) => {
-  if (date.getTime() >= Date.UTC(2021, 3, 19, 13)) {
+  if (date.getTime() >= Date.UTC(2021, 4, 3, 13)) {
+    return <Promo20210503 date={date} currencyCode={currencyCode} />;
+  } else if (date.getTime() >= Date.UTC(2021, 4, 1, 12)) {
+    return <Promo20210501 date={date} currencyCode={currencyCode} />;
+  } else if (date.getTime() >= Date.UTC(2021, 3, 19, 13)) {
     return <Promo20210419 date={date} currencyCode={currencyCode} />;
   } else if (date.getTime() >= Date.UTC(2021, 3, 17, 12)) {
     return <Promo20210417 date={date} currencyCode={currencyCode} />;
