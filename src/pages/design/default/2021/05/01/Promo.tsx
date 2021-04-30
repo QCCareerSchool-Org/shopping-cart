301516dafa907c46ffdf6f8ae7b0c4a858faee53
Dave Welsh <dave@qccareerschool.com>
2021-04-30 13:36:19 -0400

    Add design promos

---
+++ src/pages/design/default/2021/05/01/Promo.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+
+import { usePopup } from '../../../../../../hooks/usePopup';
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
+
+type Props = {
+  date: Date;
+  currencyCode: string;
+}
+
+export const Promo20210501: React.FC<Props> = ({ date, currencyCode }) => {
+  const screenWidth = useScreenWidthContext();
+  const [ popup, togglePopup ] = usePopup(false);
+
+  const desktop = screenWidth > 514;
+
+  let image: string;
+  let width: number;
+  let height: number;
+
+  if (date.getTime() >= Date.UTC(2021, 4, 2, 16)) { // May 2 at 12:00
+    if (desktop) {
+      image = require('./desktop-ends.jpg');
+    } else {
+      image = require('./mobile-ends.jpg');
+    }
+  } else {
+    if (desktop) {
+      image = require('./desktop.jpg');
+    } else {
+      image = require('./mobile.jpg');
+    }
+  }
+
+  if (desktop) {
+    width = 1257;
+    height = 532;
+  } else {
+    width = 514;
+    height = 416;
+  }
+
+  return (
+    <section id="promoSection" style={{ backgroundColor: '#001fb3', padding: 0 }}>
+      <div className="container px-0">
+        <div className="text-center">
+          <button className="btn btn-link p-0 border-0 btn-no-hover-shadow" onClick={handlePromoClick}><img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" /></button>
+        </div>
+      </div>
+      <Modal size="lg" isOpen={popup} toggle={togglePopup}>
+        <ModalHeader toggle={togglePopup}>Limited Time Offer</ModalHeader>
+        <ModalBody className="text-center">
+          <p>Receive a FREE Benjamin Moore color fan deck when you enroll in any course.</p>
+          <p>Plus, get ANY second course for free (of equal or lesser value).</p>
+        </ModalBody>
+      </Modal>
+    </section>
+  );
+
+  function handlePromoClick(event: React.MouseEvent) {
+    event.preventDefault();
+    togglePopup();
+  }
+};
