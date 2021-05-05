a03a1ac739eb1729e9f7604355fbc3b420f2467a
Dave Welsh <dave@qccareerschool.com>
2021-05-05 13:45:06 -0400

    Add event mothers' day promo

--- src/pages/event/default/2021/05/05/index.tsx
+++ src/pages/event/default/2021/05/05/index.tsx
@@ -13,7 +13,7 @@ export const Promo20210505: React.FC<Props> = ({ date, currencyCode }) => {
   const screenWidth = useScreenWidthContext();
   const [ popup, togglePopup ] = usePopup(false);
 
-  const desktop = screenWidth > 514;
+  const desktop = screenWidth > 440;
 
   let image: string;
   let width: number;
@@ -21,9 +21,9 @@ export const Promo20210505: React.FC<Props> = ({ date, currencyCode }) => {
 
   if (date.getTime() >= Date.UTC(2021, 4, 9, 4)) { // May 9 at 00:00
     if (desktop) {
-      image = currencyCode === 'GBP' ? require('./desktop-uk-ends.jpg') : require('./desktop-ends.jpg');
+      image = currencyCode === 'GBP' ? require('./desktop-ends-uk.jpg') : require('./desktop-ends.jpg');
     } else {
-      image = currencyCode === 'GBP' ? require('./mobile-uk-ends.jpg') : require('./mobile-ends.jpg');
+      image = currencyCode === 'GBP' ? require('./mobile-ends-uk.jpg') : require('./mobile-ends.jpg');
     }
   } else {
     if (desktop) {
@@ -34,32 +34,27 @@ export const Promo20210505: React.FC<Props> = ({ date, currencyCode }) => {
   }
 
   if (desktop) {
-    width = 1257;
-    height = 581;
+    width = 976;
+    height = 500;
   } else {
-    width = 514;
-    height = 486;
+    width = 440;
+    height = 442;
   }
 
   return (
-    <section id="promoSection" style={{ backgroundColor: '#be9381', padding: 0 }}>
+    <section id="promoSection" style={{ backgroundColor: '#b9b9ba', padding: 0 }}>
       <div className="container px-0">
         <div className="text-center">
-          <button className="btn btn-link p-0 border-0 btn-no-hover-shadow" onClick={handlePromoClick}><img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" /></button>
+          <button className="btn btn-link p-0 border-0 btn-no-hover-shadow" onClick={togglePopup}><img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" /></button>
         </div>
       </div>
       <Modal size="lg" isOpen={popup} toggle={togglePopup}>
         <ModalHeader toggle={togglePopup}>Special Offer</ModalHeader>
         <ModalBody className="text-center">
           <p>Celebrate Mother&apos;s Day! Receive {currencyCode === 'GBP' ? '£50' : '$50'} off your tuition when you enroll.</p>
-          <p>Plus, enroll in one of QC’s design courses and receive your second course (of equal or lesser value) for free.</p>
+          <p>Plus, enroll in one of QC&apos;s foundation courses&mdash;at our lowest deposit ever&mdash;and receive a specialty course for free (of equal or lesser value).</p>
         </ModalBody>
       </Modal>
     </section>
   );
-
-  function handlePromoClick(event: React.MouseEvent) {
-    event.preventDefault();
-    togglePopup();
-  }
 };
