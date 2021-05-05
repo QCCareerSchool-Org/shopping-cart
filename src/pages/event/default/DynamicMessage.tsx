47167dd8a4038fa4b8f26fcfe22aa18c2c2fd1dc
Dave Welsh <dave@qccareerschool.com>
2021-05-05 14:07:49 -0400

    Re-add the course-seelction alerts to event

--- src/pages/event/default/DynamicMessage.tsx
+++ src/pages/event/default/DynamicMessage.tsx
@@ -11,15 +11,5 @@ type Props = {
 }
 
 export const DynamicMessage: React.FC<Props> = ({ date, courses }) => {
-  if (date.getTime() >= Date.UTC(2021, 4, 3, 13)) {
-    return null;
-  } else if (date.getTime() >= Date.UTC(2021, 4, 1, 12)) {
-    return <DynamicMessage20210501 courses={courses} />;
-  } else if (date.getTime() >= Date.UTC(2021, 3, 19, 13)) {
-    return <DynamicMessage20210419 courses={courses} />;
-  } else if (date.getTime() >= Date.UTC(2021, 3, 6, 13)) {
-    return <DynamicMessage20210406 courses={courses} />;
-  } else {
-    return <DynamicMessage20210315 courses={courses} />;
-  }
+  return <DynamicMessage20210406 courses={courses} />;
 };
