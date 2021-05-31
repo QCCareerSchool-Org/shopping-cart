346848c7526e65c0b49df50ef8a9eb0a8a2cc715
Dave Welsh <dave@qccareerschool.com>
2021-05-31 17:10:00 -0400

    Add wellness promo

---
+++ src/pages/wellness/default/2021/06/01/index.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import React from 'react';
+
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
+
+type Props = {
+  date: Date;
+  currencyCode: string;
+};
+
+export const Promo20210601: React.FC<Props> = ({ date, currencyCode }) => {
+  const screenWidth = useScreenWidthContext();
+
+  const desktop = screenWidth > 371;
+
+  let image: string;
+  let width: number;
+  let height: number;
+
+  if (date.getTime() >= Date.UTC(2021, 5, 23, 13)) { // June 23 at 09:00
+    if (desktop) {
+      image = currencyCode === 'GBP' ? require('./desktop-ends-uk.jpg').default : require('./desktop-ends.jpg').default;
+    } else {
+      image = currencyCode === 'GBP' ? require('./mobile-ends-uk.jpg').default : require('./mobile-ends.jpg').default;
+    }
+  } else {
+    if (desktop) {
+      image = currencyCode === 'GBP' ? require('./desktop-uk.jpg').default : require('./desktop.jpg').default;
+    } else {
+      image = currencyCode === 'GBP' ? require('./mobile-uk.jpg').default : require('./mobile.jpg').default;
+    }
+  }
+
+  if (desktop) {
+    width = 960;
+    height = 469;
+  } else {
+    width = 532;
+    height = 374;
+  }
+
+  return (
+    <section id="promoSection" style={{ backgroundColor: '#ee7576', padding: 0 }}>
+      <div className="container px-0">
+        <div className="text-center">
+          <button className="btn btn-link p-0 border-0 btn-no-hover-shadow"><img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" /></button>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/wellness/default/DefaultPromo.tsx
+++ src/pages/wellness/default/DefaultPromo.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 import React from 'react';
 
-import { Promo20210302 } from './2021/03/02/';
 import { Promo20210503 } from './2021/05/03';
+import { Promo20210601 } from './2021/06/01';
 
 type Props = {
   date: Date;
@@ -10,8 +10,8 @@ type Props = {
 };
 
 export const DefaultPromo: React.FC<Props> = ({ date, currencyCode }) => {
-  if (date.getTime() >= Date.UTC(2021, 4, 3, 13)) { // May 3 at 09:00
-    return <Promo20210503 date={date} currencyCode={currencyCode} />;
+  if (date.getTime() >= Date.UTC(2021, 5, 1, 13)) { // June 1 at 09:00
+    return <Promo20210601 date={date} currencyCode={currencyCode} />;
   }
-  return <Promo20210302 date={date} currencyCode={currencyCode} />;
+  return <Promo20210503 date={date} currencyCode={currencyCode} />;
 };
