4cb29d88cbe53d810b02851a5c2928184855439b
Dave Welsh <dave@qccareerschool.com>
2021-06-11 17:01:13 -0400

    Add design promo

---
+++ src/pages/design/default/2021/06/14/index.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import React from 'react';
+import { useDispatchContext } from '../../../../../../hooks/useDispatchContext';
+
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
+import { useStateContext } from '../../../../../../hooks/useStateContext';
+
+import couponButtonAppliedSrc from './coupon-btn-SUMMER21-active.svg';
+import couponButtonSrc from './coupon-btn-SUMMER21.svg';
+
+type Props = {
+  date: Date;
+  currencyCode: string;
+};
+
+export const Promo20210614: React.FC<Props> = ({ date, currencyCode }) => {
+  const { price } = useStateContext();
+  const dispatch = useDispatchContext();
+  const screenWidth = useScreenWidthContext();
+
+  const desktop = screenWidth > 514;
+
+  let image: string;
+  let width: number;
+  let height: number;
+
+  if (date.getTime() >= Date.UTC(2021, 5, 13, 4)) { // June 13 at 00:00
+    if (desktop) {
+      image = require('./desktop-ends.jpg').default;
+    } else {
+      image = require('./mobile-ends.jpg').default;
+    }
+  } else {
+    if (desktop) {
+      image = price?.currency.code === 'GBP' ? require('./desktop-uk.jpg').default : require('./desktop.jpg').default;
+    } else {
+      image = price?.currency.code === 'GBP' ? require('./mobile-uk.jpg').default : require('./mobile.jpg').default;
+    }
+  }
+
+  if (desktop) {
+    width = 1257;
+    height = 581;
+  } else {
+    width = 514;
+    height = 486;
+  }
+
+  const buttonClick = (): void => {
+    dispatch({ type: 'SET_PROMO_CODE', payload: 'SUMMER21' });
+  };
+
+  return (
+    <>
+      <section id="promoSection" style={{ backgroundColor: '#fec901', padding: 0 }}>
+        <div className="container px-0">
+          <div className="text-center">
+            <img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" />
+          </div>
+        </div>
+      </section>
+      <div className="text-white" style={{ backgroundColor: '#fec901' }}>
+        <div className="container py-3 d-flex justify-content-center">
+          {price?.promoCode === 'SUMMER21'
+            ? <img src={couponButtonAppliedSrc} width="385" height="40" className="img-fluid" alt="Promo Code" />
+            : <button onClick={buttonClick} className="btn btn-link p-0 border-0 btn-no-hover-shadow"><img src={couponButtonSrc} width="385" height="40" className="img-fluid" alt="Promo Code" /></button>
+          }
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/design/default/DefaultPromo.tsx
+++ src/pages/design/default/DefaultPromo.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import { Promo20210601 } from './2021/06/01';
 import { Promo20210612 } from './2021/06/12';
+import { Promo20210614 } from './2021/06/14';
 
 type Props = {
   date: Date;
@@ -10,7 +11,9 @@ type Props = {
 };
 
 export const DefaultPromo: React.FC<Props> = ({ date, currencyCode }) => {
-  if (date.getTime() >= Date.UTC(2021, 5, 12, 12)) { // June 12 at 08:00
+  if (date.getTime() >= Date.UTC(2021, 5, 14, 13)) { // June 14 at 09:00
+    return <Promo20210614 date={date} currencyCode={currencyCode} />;
+  } else if (date.getTime() >= Date.UTC(2021, 5, 12, 12)) { // June 12 at 08:00
     return <Promo20210612 date={date} currencyCode={currencyCode} />;
   }
   return <Promo20210601 date={date} currencyCode={currencyCode} />;
