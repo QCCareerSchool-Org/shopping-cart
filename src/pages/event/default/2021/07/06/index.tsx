93bab28ff99ed1416d1fee3bd20d5cacfb8aba89
Dave Welsh <dave@qccareerschool.com>
2021-07-06 10:17:32 -0400

    Add DELUXE and WEDDINGSZN promos

---
+++ src/pages/event/default/2021/07/06/index.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import React from 'react';
+
+import { useDispatchContext } from '../../../../../../hooks/useDispatchContext';
+import { usePopup } from '../../../../../../hooks/usePopup';
+import { usePreloadImages } from '../../../../../../hooks/usePreloadImages';
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
+import { useStateContext } from '../../../../../../hooks/useStateContext';
+
+import couponButtonAppliedSrc from './coupon-btn-WEDDINGSZN-active.svg';
+import couponButtonSrc from './coupon-btn-WEDDINGSZN.svg';
+
+const preloadImages = [ couponButtonAppliedSrc ];
+
+type Props = {
+  date: Date;
+  currencyCode: string;
+};
+
+export const Promo20210706: React.FC<Props> = ({ date, currencyCode }) => {
+  const { price } = useStateContext();
+  const dispatch = useDispatchContext();
+  const screenWidth = useScreenWidthContext();
+
+  usePreloadImages(preloadImages);
+
+  const desktop = screenWidth > 514;
+
+  let image: string;
+  let width: number;
+  let height: number;
+
+  if (date >= new Date(2021, 6, 15, 12)) { // July 15 at 12:00
+    if (desktop) {
+      image = price?.currency.code === 'GBP' ? require('./desktop-uk-ends.jpg').default : require('./desktop-ends.jpg').default;
+    } else {
+      image = price?.currency.code === 'GBP' ? require('./mobile-uk-ends.jpg').default : require('./mobile-ends.jpg').default;
+    }
+  } else {
+    if (desktop) {
+      image = price?.currency.code === 'GBP' ? require('./desktop-uk.jpg').default : require('./desktop.jpg').default;
+    } else {
+      image = price?.currency.code === 'GBP' ? require('./mobile-uk.jpg').default : require('./mobile.jpg').default;
+    }
+  }
+
+  if (desktop) {
+    width = 976;
+    height = 436;
+  } else {
+    width = 440;
+    height = 442;
+  }
+
+  const buttonClick = (): void => {
+    dispatch({ type: 'SET_PROMO_CODE', payload: 'WEDDINGSZN' });
+  };
+
+  return (
+    <>
+      <section id="promoSection" style={{ backgroundColor: '#cc7eec', padding: 0 }}>
+        <div className="container px-0">
+          <div className="text-center">
+            <img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" />
+          </div>
+        </div>
+      </section>
+      <div className="text-white" style={{ backgroundColor: '#cc7eec' }}>
+        <div className="container py-3 d-flex justify-content-center">
+          {price?.promoCode === 'WEDDINGSZN'
+            ? <img src={couponButtonAppliedSrc} width="385" height="40" className="img-fluid" alt="Promo Code" />
+            : <button onClick={buttonClick} className="btn btn-link p-0 border-0 btn-no-hover-shadow"><img src={couponButtonSrc} width="385" height="40" className="img-fluid" alt="Promo Code" /></button>
+          }
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/event/default/DefaultPromo.tsx
+++ src/pages/event/default/DefaultPromo.tsx
@@ -1,12 +1,8 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 import React from 'react';
 
-import { Promo20210609 } from './2021/06/09';
-import { Promo20210612 } from './2021/06/12';
-import { Promo20210614 } from './2021/06/14';
-import { Promo20210618 } from './2021/06/18';
-import { Promo20210626 } from './2021/06/26';
 import { Promo20210628 } from './2021/06/28';
+import { Promo20210706 } from './2021/07/06';
 
 type Props = {
   date: Date;
@@ -14,18 +10,8 @@ type Props = {
 };
 
 export const DefaultPromo: React.FC<Props> = ({ date, currencyCode }) => {
-  if (date >= new Date(2021, 5, 28, 9, 30)) { // June 28 at 09:30
-    return <Promo20210628 date={date} currencyCode={currencyCode} />;
-  } else if (date >= new Date(2021, 5, 26, 8)) { // June 26 at 08:00
-    return <Promo20210626 date={date} currencyCode={currencyCode} />;
-  } else if (date.getTime() >= Date.UTC(2021, 5, 21, 4)) { // June 21 at 00:00
-    return <Promo20210614 date={date} currencyCode={currencyCode} />;
-  } else if (date.getTime() >= Date.UTC(2021, 5, 18, 13, 30)) { // June 18 at 09:30
-    return <Promo20210618 date={date} currencyCode={currencyCode} />;
-  } else if (date.getTime() >= Date.UTC(2021, 5, 14, 13)) { // June 14 at 09:00
-    return <Promo20210614 date={date} currencyCode={currencyCode} />;
-  } else if (date.getTime() >= Date.UTC(2021, 5, 12, 12)) { // June 12 at 08:00
-    return <Promo20210612 date={date} currencyCode={currencyCode} />;
+  if (date >= new Date(2021, 6, 6, 10, 30)) { // July 6 at 10:30
+    return <Promo20210706 date={date} currencyCode={currencyCode} />;
   }
-  return <Promo20210609 date={date} currencyCode={currencyCode} />;
+  return <Promo20210628 date={date} currencyCode={currencyCode} />;
 };
