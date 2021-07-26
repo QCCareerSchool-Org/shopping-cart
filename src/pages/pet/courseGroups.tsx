d7cf67004ab17a93d6aee21a0528f5a09bba4a3a
Dave Welsh <dave@qccareerschool.com>
2021-07-26 09:58:23 -0400

    Add disabled popups for pet

--- src/pages/pet/courseGroups.tsx
+++ src/pages/pet/courseGroups.tsx
@@ -1,11 +1,44 @@
+import React from 'react';
 import { CourseGroup } from '../../state/courses';
 
 export const courseGroups: CourseGroup[] = [
   {
     items: [
-      { code: 'DG', name: 'Dog Grooming' },
-      { code: 'FA', name: 'First Aid For Groomers' },
-      { code: 'DS', name: 'Breed Styling' },
+      {
+        code: 'DG',
+        name: 'Dog Grooming',
+        disabledMessage: (
+          <>
+            <p>The <span className="text-primary">Dog Grooming</span> course includes all the materials from the <span className="text-primary">Breed Styling</span> course.</p>
+            <p>If you would like to enroll in <span className="text-primary">Dog Grooming</span>, first <em>deselect</em> <span className="text-primary">Breed Styling</span>.</p>
+          </>
+        ),
+      },
+      {
+        code: 'FA',
+        name: 'First Aid For Groomers',
+        disabledMessage: (
+          <p>The <span className="text-primary">Breed Styling</span> course includes all the materials from the <span className="text-primary">First Aid For Groomers</span> course.</p>
+        ),
+      },
+      {
+        code: 'DS',
+        name: 'Breed Styling',
+        disabledMessage: (
+          <>
+            <p>
+              The <span className="text-primary">Dog Grooming</span> course includes all the materials from the{' '}
+              <span className="text-primary">Breed Styling</span> course and the <span className="text-primary">Breed Styling</span>{' '}
+              course includes all the materials from the <span className="text-primary">First Aid For Groomers</span> course.
+            </p>
+            <p>
+              If you would like to take <span className="text-primary">Breed Styling</span>, make sure both{' '}
+              the <span className="text-primary">Dog Grooming</span> and <span className="text-primary">First Aid For Groomers</span>{' '}
+              courses are deselected.
+            </p>
+          </>
+        ),
+      },
     ],
   },
 ];
