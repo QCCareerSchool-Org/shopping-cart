7598c396b738039f69bef9622497e48282010b03
Dave Welsh <dave@qccareerschool.com>
2021-08-03 14:03:23 -0400

    Add event promotion

---
+++ src/pages/event/default/2021/08/03/index.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import React from 'react';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+
+import { PromoCode } from '../../../../../../components/PromoCode';
+import { useDispatchContext } from '../../../../../../hooks/useDispatchContext';
+import { usePopup } from '../../../../../../hooks/usePopup';
+import { usePreloadImages } from '../../../../../../hooks/usePreloadImages';
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
+import { useStateContext } from '../../../../../../hooks/useStateContext';
+
+import couponButtonAppliedSrc from './button-TRIPLETHREAT-active.svg';
+import couponButtonSrc from './button-TRIPLETHREAT.svg';
+
+const preloadImages = [ couponButtonAppliedSrc ];
+
+type Props = {
+  date: Date;
+};
+
+export const Promo20210803: React.FC<Props> = ({ date }) => {
+  const { price } = useStateContext();
+  const dispatch = useDispatchContext();
+  const screenWidth = useScreenWidthContext();
+  const [ popup, togglePopup ] = usePopup(false);
+
+  usePreloadImages(preloadImages);
+
+  const desktop = screenWidth > 514;
+
+  let image: string;
+  let width: number;
+  let height: number;
+
+  if (date.getTime() >= Date.UTC(2021, 8, 12, 4)) { // August 12 at 00:00 (04:00 GMT)
+    if (desktop) {
+      image = require('./desktop-ends.jpg').default;
+    } else {
+      image = require('./mobile-ends.jpg').default;
+    }
+  } else {
+    if (desktop) {
+      image = require('./desktop.jpg').default;
+    } else {
+      image = require('./mobile.jpg').default;
+    }
+  }
+
+  if (desktop) {
+    width = 976;
+    height = 436;
+  } else {
+    width = 600;
+    height = 732;
+  }
+
+  const buttonClick = (): void => {
+    dispatch({ type: 'SET_PROMO_CODE', payload: 'TRIPLETHREAT' });
+  };
+
+  return (
+    <>
+      <section id="promoSection" style={{ backgroundColor: '#fff', padding: 0 }}>
+        <div className="container px-0">
+          <div className="text-center">
+            <button className="btn btn-link p-0 border-0 btn-no-hover-shadow" onClick={togglePopup}>
+              <img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" />
+            </button>
+          </div>
+        </div>
+        <Modal size="lg" isOpen={popup} toggle={togglePopup}>
+          <ModalHeader toggle={togglePopup}>Special Offer</ModalHeader>
+          <ModalBody className="text-center">
+            When you enroll in any of QC&apos;s Foundation courses, use promo code <PromoCode>TRIPLETHREAT</PromoCode> to get any two <strong>Specialty</strong> courses FREE!
+          </ModalBody>
+        </Modal>
+      </section>
+      <div className="text-white" style={{ backgroundColor: '#fff' }}>
+        <div className="container py-3 d-flex justify-content-center">
+          {price?.promoCode === 'TRIPLETHREAT'
+            ? <img src={couponButtonAppliedSrc} width="297" height="40" className="img-fluid" alt="Promo Code" />
+            : <button onClick={buttonClick} className="btn btn-link p-0 border-0 btn-no-hover-shadow"><img src={couponButtonSrc} width="297" height="40" className="img-fluid" alt="Promo Code" /></button>
+          }
+        </div>
+      </div>
+    </>
+  );
+};
