321fbe732684c62bbf89ff13befac0f8ec940287
Dave Welsh <dave@qccareerschool.com>
2021-08-19 11:04:29 -0400

    Add wellness promo and update pet popup

--- src/pages/wellness/default/DefaultPromo.tsx
+++ src/pages/wellness/default/DefaultPromo.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 import React from 'react';
 
-import { Promo20210803 } from './2021/08/03';
+import { Promo20210819 } from './2021/08/19';
 
 type Props = {
   date: Date;
 };
 
 export const DefaultPromo: React.FC<Props> = ({ date }) => {
-  return <Promo20210803 date={date} />;
+  return <Promo20210819 date={date} />;
 };
