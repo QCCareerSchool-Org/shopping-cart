b56cad9ea7a7b9ac6e6b29b41cda003fa16695ea
Dave Welsh <dave@qccareerschool.com>
2021-09-07 09:46:45 -0400

    Add wellness and pet promotions

--- src/pages/pet/default/DefaultPromo.tsx
+++ src/pages/pet/default/DefaultPromo.tsx
@@ -1,12 +1,16 @@
-/* eslint-disable @typescript-eslint/no-magic-numbers */
 import React from 'react';
 
 import { Promo20210830 } from './2021/08/30';
+import { Promo20210907 } from './2021/09/07';
 
 type Props = {
   date: Date;
 };
 
 export const DefaultPromo: React.FC<Props> = ({ date }) => {
+  const time = date.getTime();
+  if (time >= Date.UTC(2021, 8, 7, 13, 30)) { // September 7 at 09:30 (13:30 UTC)
+    return <Promo20210907 date={date} />;
+  }
   return <Promo20210830 date={date} />;
 };
