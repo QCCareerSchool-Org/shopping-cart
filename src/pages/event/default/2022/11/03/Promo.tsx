534ce07ebef80134c60e277f50cf8bf03d809ea4
Dave Welsh <dave@qccareerschool.com>
2022-11-03 10:38:55 -0400

    Add event 2022-11-03 offer

---
+++ src/pages/event/default/2022/11/03/Promo.tsx
@@ -0,0 +1,86 @@
+import React, { FC } from 'react';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { CountDownTimerWrapper } from '../../../../../../components/CountDownTimerWrapper';
+import { useDateContext } from '../../../../../../hooks/useDateContext';
+
+import { usePopup } from '../../../../../../hooks/usePopup';
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
+import { useStateContext } from '../../../../../../hooks/useStateContext';
+import { dateOverride } from '../../../../../../lib/dateOverride';
+
+const potentialSavings = (currencyCode: string): string => {
+  return currencyCode === 'GBP'
+    ? '£778'
+    : currencyCode === 'AUD'
+      ? '$1289'
+      : currencyCode === 'NZD'
+        ? '$1358'
+        : '$1039';
+};
+
+const lastChanceGraphicsDate = new Date(Date.UTC(2022, 10, 7, 5)); // November 7 at 00:00 (05:00 UTC)
+const timerShowDate = new Date(Date.UTC(2022, 10, 7, 5)); // November 7 at 00:00 (05:00 UTC)
+const timerEndDate = new Date(Date.UTC(2022, 10, 12, 5)); // November 12 at 00:00 (05:00 UTC)
+
+export const EventPromo20221103: FC = () => {
+  const serverDate = useDateContext();
+  const date = dateOverride() ?? serverDate;
+  const { price } = useStateContext();
+  const screenWidth = useScreenWidthContext();
+  const [ popup, togglePopup ] = usePopup(false);
+
+  const desktop = screenWidth >= 576;
+
+  let image: string;
+  let width: number;
+  let height: number;
+
+  if (date.getTime() >= lastChanceGraphicsDate.getTime()) {
+    if (desktop) {
+      image = price?.currency.code === 'GBP' ? require('./desktop-ends-uk.jpg') : require('./desktop-ends.jpg');
+    } else {
+      image = price?.currency.code === 'GBP' ? require('./mobile-ends-uk.jpg') : require('./mobile-ends.jpg');
+    }
+  } else {
+    if (desktop) {
+      image = price?.currency.code === 'GBP' ? require('./desktop-uk.jpg') : require('./desktop.jpg');
+    } else {
+      image = price?.currency.code === 'GBP' ? require('./mobile-uk.jpg') : require('./mobile.jpg');
+    }
+  }
+
+  if (desktop) {
+    width = 976;
+    height = 500;
+  } else {
+    width = 440;
+    height = 500;
+  }
+
+  return (
+    <>
+      <section id="promoSection" style={{ backgroundColor: '#a4cb96', padding: 0 }}>
+        <div className="container text-center px-0">
+          <button className="btn btn-link p-0 border-0 btn-no-hover-shadow" onClick={togglePopup}>
+            <img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" />
+          </button>
+        </div>
+        <Modal isOpen={popup} toggle={togglePopup}>
+          <ModalHeader toggle={togglePopup}>Get a FREE specialty course!</ModalHeader>
+          <ModalBody>
+            <p>Enroll in any foundation course and get a FREE specialty course of your choice. You can get started today for only {price?.currency.code === 'GBP' ? '£49' : '$49'}!</p>
+          </ModalBody>
+        </Modal>
+      </section>
+      <CountDownTimerWrapper
+        date={date}
+        showDate={timerShowDate}
+        endDate={timerEndDate}
+        buttonInverse={true}
+        className="text-white"
+        style={{ backgroundColor: 'black' }}
+        message={<span style={{ textTransform: 'uppercase' }}>This exclusive offer <strong className="endHighlight">ends {date.getTime() >= timerEndDate.getTime() - (1000 * 60 * 60 * 24) ? 'today' : 'soon'}</strong></span>}
+      />
+    </>
+  );
+};
