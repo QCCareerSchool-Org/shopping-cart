12c6ca22c0496e627a69041fc4e802620d5d47b3
Dave Welsh <dave@qccareerschool.com>
2022-12-12 10:00:39 -0500

    Add event promo

---
+++ src/pages/event/default/2022/12/12/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+
+import { BuyOneGetOne } from '../../../../../../components/BuyOneGetOne';
+import { Form } from '../../../../../../components/Form';
+import { courseGroups } from '../../../../courseGroups';
+import { Guarantee } from '../../../../Guarantee';
+import { TwoFreeSpecialtyDynamicMessage } from '../../../../TwoFreeSpecialtyDynamicMessage';
+import { EventPromo20221212 } from './Promo';
+
+export const Event20221212: FC = () => (
+  <>
+    <EventPromo20221212 />
+    <Form
+      courseGroups={courseGroups}
+      school="QC Event School"
+      guarantee={() => <Guarantee />}
+      coursesSubtitle={() => <BuyOneGetOne />}
+      agreementLink="https://www.qceventplanning.com/enrollment-agreement.html"
+      agreementLinkGB="https://www.qceventplanning.com/enrollment-agreement-gb.html"
+      successLink="https://www.qceventplanning.com/welcome-to-the-school/"
+      dynamicCourseMessages={[ () => <TwoFreeSpecialtyDynamicMessage /> ]}
+      promoCodeDefault="2SPECIALTY100"
+    />
+  </>
+);
