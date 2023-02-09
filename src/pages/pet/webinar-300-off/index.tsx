3844f6645830b7f1de662806361a556408836619
Dave Welsh <dave@qccareerschool.com>
2023-02-09 13:26:07 -0500

    Add webinar cart

---
+++ src/pages/pet/webinar-300-off/index.tsx
@@ -0,0 +1,45 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { ReactElement } from 'react';
+
+import { Form } from '../../../components/Form';
+import { BOGODynamicMessage } from '../BOGODynamicMessage';
+import { courseGroups } from '../courseGroups';
+import { Guarantee } from '../Guarantee';
+import { PetCoursesSubtitle } from '../PetCoursesSubtitle';
+import { Promo } from './promo';
+
+const Webinar300Off = (): ReactElement => (
+  <>
+    <Promo />
+    <Form
+      courseGroups={courseGroups}
+      school="QC Pet Studies"
+      guarantee={() => <Guarantee />}
+      coursesSubtitle={() => <PetCoursesSubtitle />}
+      agreementLink="https://www.qcpetstudies.com/enrollment-agreement"
+      agreementLinkGB="https://www.qcpetstudies.com/enrollment-agreement-gb"
+      successLink="https://www.qcpetstudies.com/welcome-to-the-school"
+      promoCodeDefault="PET300OFF"
+      dynamicCourseMessages={[ () => <BOGODynamicMessage /> ]}
+    />
+  </>
+);
+
+interface DynamicMessageProps {
+  courses: string[];
+  currencyCode: string;
+}
+
+const DynamicMessage = ({ courses, currencyCode }: DynamicMessageProps): ReactElement | null => {
+  const discount = currencyCode === 'GBP' ? '£300' : '$300';
+  if (courses.length === 0) {
+    return null;
+  } else if (!courses.includes('DT')) {
+    return <p className="mt-4 alert alert-warning"><FontAwesomeIcon icon={faInfoCircle} /> Select the <strong>Dog Training</strong> course to get {discount} off your tuition</p>;
+  }
+  return <p className="mt-4 alert alert-success"><FontAwesomeIcon icon={faCheckCircle} /> Discount applied!</p>;
+};
+
+export default Webinar300Off;
