1b4f5e00e095c9ee2854a54930eaac2339c3e62a
Dave Welsh <dave@qccareerschool.com>
2023-02-16 16:42:29 -0500

    Update image and popup for pet webinar cart

--- src/pages/pet/webinar-300-off/promo.tsx
+++ src/pages/pet/webinar-300-off/promo.tsx
@@ -1,9 +1,14 @@
 import React, { ReactElement } from 'react';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { usePopup } from '../../../hooks/usePopup';
 
 import { useScreenWidthContext } from '../../../hooks/useScreenWidthContext';
+import { useStateContext } from '../../../hooks/useStateContext';
 
 export const Promo = (): ReactElement => {
   const screenWidth = useScreenWidthContext();
+  const { price } = useStateContext();
+  const [ popup, togglePopup ] = usePopup(false);
 
   const desktop = screenWidth >= 576;
 
@@ -11,12 +16,12 @@ export const Promo = (): ReactElement => {
   let height: number;
 
   const image = desktop
-    ? require('./desktop.jpg')
-    : require('./mobile.jpg');
+    ? price?.currency.code === 'GBP' ? require('./desktop-uk.jpg') : require('./desktop.jpg')
+    : price?.currency.code === 'GBP' ? require('./mobile-uk.jpg') : require('./mobile.jpg');
 
   if (desktop) {
     width = 960;
-    height = 400;
+    height = 430;
   } else {
     width = 518;
     height = 566;
@@ -29,6 +34,13 @@ export const Promo = (): ReactElement => {
           <img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" />
         </div>
       </div>
+      <Modal size="md" isOpen={popup} toggle={togglePopup}>
+        <ModalHeader toggle={togglePopup}>Get {price?.currency.code === 'GBP' ? '£300' : '$300'} off your tuition!</ModalHeader>
+        <ModalBody>
+          <p className="lead">Take advantage of this special offer and claim {price?.currency.code === 'GBP' ? '£300' : '$300'} off your dog grooming, training, or daycare tuition.</p>
+          <p className="lead">You can get started today for only {price?.currency.code === 'GBP' ? '£99' : '$99'}!</p>
+        </ModalBody>
+      </Modal>
     </section>
   );
 };
