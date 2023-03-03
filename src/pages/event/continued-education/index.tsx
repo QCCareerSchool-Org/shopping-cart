dfcd068ff4b26ee217343dc560ea5a9dbb5aa6fa
Dave Welsh <dave@qccareerschool.com>
2023-03-03 15:41:15 -0500

    Add promo sections to continued ed pages

--- src/pages/event/continued-education/index.tsx
+++ src/pages/event/continued-education/index.tsx
@@ -3,15 +3,19 @@ import React, { FC } from 'react';
 import { Form } from '../../../components/Form';
 import { courseGroups } from '../courseGroups';
 import { Guarantee } from '../Guarantee';
+import { ContinuedEducationPromo } from './Promo';
 
 export const EventContinuedEducation: FC = () => (
-  <Form
-    courseGroups={courseGroups}
-    school="QC Event School"
-    guarantee={() => <Guarantee />}
-    agreementLink="https://www.qceventplanning.com/enrollment-agreement.html"
-    agreementLinkGB="https://www.qceventplanning.com/enrollment-agreement-gb.html"
-    successLink="https://www.qceventplanning.com/welcome-to-the-school/"
-    student={true}
-  />
+  <>
+    <ContinuedEducationPromo />
+    <Form
+      courseGroups={courseGroups}
+      school="QC Event School"
+      guarantee={() => <Guarantee />}
+      agreementLink="https://www.qceventplanning.com/enrollment-agreement.html"
+      agreementLinkGB="https://www.qceventplanning.com/enrollment-agreement-gb.html"
+      successLink="https://www.qceventplanning.com/welcome-to-the-school/"
+      student={true}
+    />
+  </>
 );
--- src/pages/makeup/continued-education/index.tsx
+++ src/pages/makeup/continued-education/index.tsx
@@ -3,15 +3,19 @@ import React, { FC } from 'react';
 import { Form } from '../../../components/Form';
 import { courseGroups } from '../courseGroups';
 import { Guarantee } from '../Guarantee';
+import { ContinuedEducationPromo } from './Promo';
 
 export const MakeupContinuedEducation: FC = () => (
-  <Form
-    courseGroups={courseGroups}
-    school="QC Makeup Academy"
-    guarantee={() => <Guarantee />}
-    agreementLink="https://www.qcmakeupacademy.com/enrollment-agreement.html"
-    agreementLinkGB="https://www.qcmakeupacademy.com/enrollment-agreement-gb.html"
-    successLink="https://www.qcmakeupacademy.com/welcome-to-the-school/"
-    student={true}
-  />
+  <>
+    <ContinuedEducationPromo />
+    <Form
+      courseGroups={courseGroups}
+      school="QC Makeup Academy"
+      guarantee={() => <Guarantee />}
+      agreementLink="https://www.qcmakeupacademy.com/enrollment-agreement.html"
+      agreementLinkGB="https://www.qcmakeupacademy.com/enrollment-agreement-gb.html"
+      successLink="https://www.qcmakeupacademy.com/welcome-to-the-school/"
+      student={true}
+    />
+  </>
 );
