cc99cad3366aefb48bfef0801818dbbd9f0a3a0f
Dave Welsh <dave@qccareerschool.com>
2023-04-13 09:22:20 -0400

    Change end date to the 17th

--- src/pages/makeup/default/index.tsx
+++ src/pages/makeup/default/index.tsx
@@ -16,7 +16,7 @@ export const MakeupDefault: FC = () => {
     return <Suspense fallback={null}><Makeup20230322 /></Suspense>;
   }
 
-  if (time >= Date.UTC(2023, 3, 6, 13, 30) && time < Date.UTC(2023, 3, 15, 4)) { // 2023-04-06T09:30 (13:30 UTC) to 2023-04-15T00:00 (04:00 UTC)
+  if (time >= Date.UTC(2023, 3, 6, 13, 30) && time < Date.UTC(2023, 3, 17, 4)) { // 2023-04-06T09:30 (13:30 UTC) to 2023-04-17T00:00 (04:00 UTC)
     return <Suspense fallback={null}><Makeup20230406 /></Suspense>;
   }
 
