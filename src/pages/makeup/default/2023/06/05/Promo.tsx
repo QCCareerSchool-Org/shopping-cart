89ae088db673f983b491309ad96df31aa4c9efc9
Dave Welsh <dave@qccareerschool.com>
2023-06-02 16:10:04 -0400

    Update images

--- src/pages/makeup/default/2023/06/05/Promo.tsx
+++ src/pages/makeup/default/2023/06/05/Promo.tsx
@@ -10,16 +10,20 @@ import { dateOverride } from '../../../../../../lib/dateOverride';
 const lastChanceDate = new Date(Date.UTC(2023, 5, 9, 4)); // June 9 at 00:00 (04:00 UTC)
 const timerShowDate = new Date(Date.UTC(2023, 5, 9, 4)); // June 9 at 00:00 (04:00 UTC)
 const timerEndDate = new Date(Date.UTC(2023, 5, 13, 4)); // June 13 at 00:00 (04:00 UTC)
-const backgroundColor = '#efbab4';
+const backgroundColor = '#f4f4f4';
 
-const getImageData = (desktop: boolean, lastChance: boolean): { image: any; width: number; height: number } => {
+const getImageData = (desktop: boolean, lastChance: boolean, currencyCode?: string): { image: any; width: number; height: number } => {
   const [ width, height ] = desktop ? [ 1060, 541 ] : [ 600, 656 ];
 
   if (lastChance) {
-    const image = desktop ? require('./desktop-ends.jpg') : require('./mobile-ends.jpg');
+    const image = desktop
+      ? currencyCode === 'GBP' ? require('./desktop-uk-ends.jpg') : require('./desktop-ends.jpg')
+      : currencyCode === 'GBP' ? require('./mobile-uk-ends.jpg') : require('./mobile-ends.jpg');
     return { image, width, height };
   }
-  const image = desktop ? require('./desktop.jpg') : require('./mobile.jpg');
+  const image = desktop
+    ? currencyCode === 'GBP' ? require('./desktop-uk.jpg') : require('./desktop.jpg')
+    : currencyCode === 'GBP' ? require('./mobile-uk.jpg') : require('./mobile.jpg');
   return { image, width, height };
 };
 
