98de624d1c780a7b79d9b7e9dceff6632d449c0a
Dave Welsh <dave@qccareerschool.com>
2023-06-02 12:24:15 -0400

    Added June 6 promotions

---
+++ src/pages/makeup/default/2023/06/05/Promo.tsx
@@ -0,0 +1,67 @@
+import React, { ReactElement } from 'react';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { CountDownTimerWrapper } from '../../../../../../components/CountDownTimerWrapper';
+import { useDateContext } from '../../../../../../hooks/useDateContext';
+
+import { usePopup } from '../../../../../../hooks/usePopup';
+import { useScreenWidthContext } from '../../../../../../hooks/useScreenWidthContext';
+import { dateOverride } from '../../../../../../lib/dateOverride';
+
+const lastChanceDate = new Date(Date.UTC(2023, 5, 9, 4)); // June 9 at 00:00 (04:00 UTC)
+const timerShowDate = new Date(Date.UTC(2023, 5, 9, 4)); // June 9 at 00:00 (04:00 UTC)
+const timerEndDate = new Date(Date.UTC(2023, 5, 13, 4)); // June 13 at 00:00 (04:00 UTC)
+const backgroundColor = '#efbab4';
+
+const getImageData = (desktop: boolean, lastChance: boolean): { image: any; width: number; height: number } => {
+  const [ width, height ] = desktop ? [ 1060, 541 ] : [ 600, 656 ];
+
+  if (lastChance) {
+    const image = desktop ? require('./desktop-ends.jpg') : require('./mobile-ends.jpg');
+    return { image, width, height };
+  }
+  const image = desktop ? require('./desktop.jpg') : require('./mobile.jpg');
+  return { image, width, height };
+};
+
+export const MakeupPromo20230605 = (): ReactElement => {
+  const serverDate = useDateContext();
+  const date = dateOverride() ?? serverDate;
+  const screenWidth = useScreenWidthContext();
+  const [ popup, togglePopup ] = usePopup(false);
+
+  const desktop = screenWidth >= 576;
+
+  const { image, width, height } = getImageData(desktop, date >= lastChanceDate);
+
+  return (
+    <>
+      <section id="promoSection" style={{ backgroundColor, padding: 0 }}>
+        <div className="container text-center px-0">
+          <button className="btn btn-link p-0 border-0 btn-no-hover-shadow" onClick={togglePopup}>
+            <img src={image} width={width} height={height} className="img-fluid d-block mx-auto" alt="Special Offer" />
+          </button>
+        </div>
+        <Modal isOpen={popup} toggle={togglePopup}>
+          <ModalHeader toggle={togglePopup}>FREE Pro Makeup Workshop</ModalHeader>
+          <ModalBody>
+            <p>Enroll in <strong>Master Makeup Artistry</strong> and get the <strong>Pro Makeup Workshop</strong> FREE! When you pay in full, you&apos;ll also get the entire Deluxe Collection!</p>
+            <div style={{ display: 'flex', justifyContent: 'center' }}><img src={require('./enrollment-pop-up.jpg')} width="650" height="1056" className="img-fluid" /></div>
+          </ModalBody>
+          <ModalFooter>
+            <p className="small">Your items will be automatically sent to you after you have submitted Unit A of the course in the Online Student Center. Items in the kit are subject to change.</p>
+            <p className="small mb-0">The DELUXE kit is not required for you to complete your assignments and will not determine your success in the course.</p>
+          </ModalFooter>
+        </Modal>
+      </section>
+      <CountDownTimerWrapper
+        date={date}
+        showDate={timerShowDate}
+        endDate={timerEndDate}
+        buttonInverse={true}
+        className="text-white"
+        style={{ backgroundColor: 'black' }}
+        message={<span style={{ textTransform: 'uppercase' }}><strong>Last chance!</strong> Get the Pro Makeup Workshop FREE!</span>}
+      />
+    </>
+  );
+};
--- src/pages/wellness/default/index.tsx
+++ src/pages/wellness/default/index.tsx
@@ -4,15 +4,15 @@ import { useDateContext } from '../../../hooks/useDateContext';
 import { dateOverride } from '../../../lib/dateOverride';
 
 const WellnessFallback = lazy(async () => import('./fallback').then(m => ({ default: m.WellnessFallback })));
-const Wellness20230522 = lazy(async () => import('./2023/05/22').then(m => ({ default: m.Wellness20230522 })));
+const Wellness20230605 = lazy(async () => import('./2023/06/05').then(m => ({ default: m.Wellness20230605 })));
 
 export const WellnessDefault = (): ReactElement => {
   const serverDate = useDateContext();
   const date = dateOverride() ?? serverDate;
   const time = date.getTime();
 
-  if (time >= Date.UTC(2023, 4, 22, 13, 30) && time < Date.UTC(2023, 5, 1, 4)) { // 2023-05-22T09:30 (13:30 UTC) to 2023-06-01T00:00 (04:00 UTC)
-    return <Suspense fallback={null}><Wellness20230522 /></Suspense>;
+  if (time >= Date.UTC(2023, 5, 5, 13, 30) && time < Date.UTC(2023, 5, 13, 4)) { // 2023-06-05T09:30 (13:30 UTC) to 2023-06-13T00:00 (04:00 UTC)
+    return <Suspense fallback={null}><Wellness20230605 /></Suspense>;
   }
 
   return <Suspense fallback={null}><WellnessFallback /></Suspense>;
