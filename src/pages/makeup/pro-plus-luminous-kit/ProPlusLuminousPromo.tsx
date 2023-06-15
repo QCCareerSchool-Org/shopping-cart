b14973fdb54370ac5913a1fe5846903f431b2e6c
Dave Welsh <dave@qccareerschool.com>
2023-06-15 12:54:57 -0400

    Update free skincare and pro makeup workshop promos

--- src/pages/makeup/pro-plus-luminous-kit/ProPlusLuminousPromo.tsx
+++ src/pages/makeup/pro-plus-luminous-kit/ProPlusLuminousPromo.tsx
@@ -2,17 +2,20 @@
 import React, { memo } from 'react';
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
+import styles from '../../../components/coloredList.module.css';
 import { usePopup } from '../../../hooks/usePopup';
 import { useScreenWidthContext } from '../../../hooks/useScreenWidthContext';
+import { useStateContext } from '../../../hooks/useStateContext';
 
 export const ProPlusLuminousPromo = memo(() => {
   const screenWidth = useScreenWidthContext();
   const [ popup, togglePopup ] = usePopup(false);
+  const { price } = useStateContext();
 
   const desktop = screenWidth > 500;
 
-  const desktopImage = require('./desktop.jpg');
-  const mobileImage = require('./mobile.jpg');
+  const desktopImage = price?.currency.code === 'GBP' ? require('./desktop-uk.jpg') : require('./desktop.jpg');
+  const mobileImage = price?.currency.code === 'GBP' ? require('./mobile-uk.jpg') : require('./mobile.jpg');
 
   return (
     <section id="promoSection" style={{ padding: 0, backgroundColor: '#131f2e' }}>
@@ -26,14 +29,32 @@ export const ProPlusLuminousPromo = memo(() => {
       <Modal isOpen={popup} toggle={togglePopup}>
         <ModalHeader toggle={togglePopup}>Limited-Time Offer</ModalHeader>
         <ModalBody>
+          <h4>Free Pro Makeup Workshop</h4>
           <p>When you enroll in <strong>Master Makeup Artistry</strong>, you&apos;ll get QC&apos;s <strong>Pro Makeup Workshop</strong> for free!</p>
-          <p>That&apos;s not all: if you choose the pay-in-full payment plan we&apos;ll also send you the entire <strong>DELUXE Kit with 17-piece brush set</strong>. Part payers will receive the <strong>17-piece brush set</strong>.</p>
+          <h4>Free DELUXE Collection</h4>
+          <p>Get the entire <strong>DELUXE Kit with 17-piece brush set</strong> when you enroll in <strong>Master Makeup Artistry</strong>.</p>
           <p>Graduate as a Master International Makeup Professional™ (MIMP™) in just a few short months and build your beauty empire!</p>
-          <div style={{ display: 'flex', justifyContent: 'center' }}><img src={require('./enrollment-pop-up.jpg')} className="img-fluid" /></div>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div>
+              <img src={require('../../../images/deluxe-kit-numbers-no-description.jpg')} width="650" height="1056" className="img-fluid" />
+            </div>
+            <ol className={styles.coloredList}>
+              <li>17-piece professional brush set</li>
+              <li>88-shade eye shadow palette</li>
+              <li>32-shade lip palette</li>
+              <li>28-shade blush palette</li>
+              <li>20-shade conceal &amp; correct palette</li>
+              <li>9-shade contour palette</li>
+              <li>4-shade highlight palette</li>
+              <li>5-shade eyebrow palette</li>
+              <li>4 sets of faux lashes</li>
+              <li>Pro palette &amp; spatula</li>
+            </ol>
+          </div>
         </ModalBody>
         <ModalFooter>
           <p className="small">Your items will be automatically sent to you after you have submitted Unit A of the course in the Online Student Center. Items in the kit are subject to change.</p>
-          <p className="small mb-0">The DELUXE kit is not required for you to complete your assignments and will not determine your success in the course.</p>
+          <p className="small mb-0">The DELUXE Collection is not required for you to complete your assignments and will not determine your success in the course.</p>
         </ModalFooter>
       </Modal>
     </section>
