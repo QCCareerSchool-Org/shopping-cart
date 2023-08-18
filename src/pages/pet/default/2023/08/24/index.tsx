0090fdddc2cda255d41fdb6afb8de242b8ba67ca
Dave Welsh <dave@qccareerschool.com>
2023-08-18 14:50:27 -0400

    Added Aug 24 promotions

---
+++ src/pages/pet/default/2023/08/24/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+
+import { Form } from '../../../../../../components/Form';
+import { BOGODynamicMessage } from '../../../../BOGODynamicMessage';
+import { courseGroups } from '../../../../courseGroups';
+import { Guarantee } from '../../../../Guarantee';
+import { PetCoursesSubtitle } from '../../../../PetCoursesSubtitle';
+import { PetPromo20230824 } from './Promo';
+
+export const Pet20230824: FC = () => (
+  <>
+    <PetPromo20230824 />
+    <Form
+      courseGroups={courseGroups}
+      school="QC Pet Studies"
+      guarantee={() => <Guarantee />}
+      coursesSubtitle={() => <PetCoursesSubtitle />}
+      agreementLink="https://www.qcpetstudies.com/enrollment-agreement"
+      agreementLinkGB="https://www.qcpetstudies.com/enrollment-agreement-gb"
+      successLink="https://www.qcpetstudies.com/welcome-to-the-school"
+      dynamicCourseMessages={[ () => <BOGODynamicMessage /> ]}
+      dynamicCourseDescriptions="SHOW"
+      promoCodeDefault="PET300OFF"
+      billingAddress={true}
+    />
+  </>
+);
--- src/pages/pet/default/index.tsx
+++ src/pages/pet/default/index.tsx
@@ -4,15 +4,15 @@ import { useDateContext } from '../../../hooks/useDateContext';
 import { dateOverride } from '../../../lib/dateOverride';
 
 const PetFallback = React.lazy(async () => import('./fallback').then(m => ({ default: m.PetFallback })));
-const Pet20230807 = React.lazy(async () => import('./2023/08/07').then(m => ({ default: m.Pet20230807 })));
+const Pet20230824 = React.lazy(async () => import('./2023/08/24').then(m => ({ default: m.Pet20230824 })));
 
 export const PetDefault: React.FC = () => {
   const serverDate = useDateContext();
   const date = dateOverride() ?? serverDate;
   const time = date.getTime();
 
-  if (time >= Date.UTC(2023, 7, 7, 13, 30) && time < Date.UTC(2023, 7, 17, 4)) { // 2023-08-07T09:30 (13:30 UTC) to 2023-08-17T00:00 (04:00 UTC)
-    return <Suspense fallback={null}><Pet20230807 /></Suspense>;
+  if (time >= Date.UTC(2023, 7, 24, 13, 30) && time < Date.UTC(2023, 8, 9, 4)) { // 2023-08-24T09:30 (13:30 UTC) to 2023-09-09T00:00 (04:00 UTC)
+    return <Suspense fallback={null}><Pet20230824 /></Suspense>;
   }
 
   return <Suspense fallback={null}><PetFallback /></Suspense>;
