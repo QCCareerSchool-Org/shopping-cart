97c36ed2abfa4f1c1bd7ce54897301fd7f1f7d72
Dave Welsh <dave@qccareerschool.com>
2023-10-20 16:25:21 -0400

    Added the October 23 promotions

--- src/pages/design/default/index.tsx
+++ src/pages/design/default/index.tsx
@@ -4,15 +4,15 @@ import { useDateContext } from '../../../hooks/useDateContext';
 import { dateOverride } from '../../../lib/dateOverride';
 
 const DesignFallback = lazy(async () => import('./fallback').then(m => ({ default: m.DesignFallback })));
-const Design20231009 = lazy(async () => import('./2023/10/09').then(m => ({ default: m.Design20231009 })));
+const Design20231023 = lazy(async () => import('./2023/10/23').then(m => ({ default: m.Design20231023 })));
 
 export const DesignDefault: FC = () => {
   const serverDate = useDateContext();
   const date = dateOverride() ?? serverDate;
   const time = date.getTime();
 
-  if (time >= Date.UTC(2023, 9, 9, 13, 30) && time < Date.UTC(2023, 9, 16, 4)) { // 2023-10-09T09:30 (13:30 UTC) to 2023-10-16T00:00 (04:00 UTC)
-    return <Suspense fallback={null}><Design20231009 /></Suspense>;
+  if (time >= Date.UTC(2023, 9, 23, 13, 30) && time < Date.UTC(2023, 10, 1, 4)) { // 2023-10-23T09:30 (13:30 UTC) to 2023-1-01T00:00 (04:00 UTC)
+    return <Suspense fallback={null}><Design20231023 /></Suspense>;
   }
 
   return <Suspense fallback={null}><DesignFallback /></Suspense>;
--- src/pages/wellness/default/index.tsx
+++ src/pages/wellness/default/index.tsx
@@ -4,15 +4,15 @@ import { useDateContext } from '../../../hooks/useDateContext';
 import { dateOverride } from '../../../lib/dateOverride';
 
 const WellnessFallback = lazy(async () => import('./fallback').then(m => ({ default: m.WellnessFallback })));
-const Wellness20231009 = lazy(async () => import('./2023/10/09').then(m => ({ default: m.Wellness20231009 })));
+const Wellness20231023 = lazy(async () => import('./2023/10/23').then(m => ({ default: m.Wellness20231023 })));
 
 export const WellnessDefault = (): ReactElement => {
   const serverDate = useDateContext();
   const date = dateOverride() ?? serverDate;
   const time = date.getTime();
 
-  if (time >= Date.UTC(2023, 9, 9, 13, 30) && time < Date.UTC(2023, 9, 16, 4)) { // 2023-10-09T09:30 (13:30 UTC) to 2023-10-16T00:00 (04:00 UTC)
-    return <Suspense fallback={null}><Wellness20231009 /></Suspense>;
+  if (time >= Date.UTC(2023, 9, 23, 13, 30) && time < Date.UTC(2023, 10, 1, 4)) { // 2023-10-23T09:30 (13:30 UTC) to 2023-1-01T00:00 (04:00 UTC)
+    return <Suspense fallback={null}><Wellness20231023 /></Suspense>;
   }
 
   return <Suspense fallback={null}><WellnessFallback /></Suspense>;
