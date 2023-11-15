cf6ec1cafadab8eb8d6dd5a9f14a18da033cc53d
Dave Welsh <dave@qccareerschool.com>
2023-11-15 10:04:26 -0500

    Added makeup Black Friday and Cyber Monday promotions

---
+++ src/pages/makeup/default/2023/11/16/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import { BogoDynamicMessage } from '../../../../../../components/BogoDynamicMessage';
+
+import { Form } from '../../../../../../components/Form';
+import { courseGroups } from '../../../../courseGroups';
+import { Guarantee } from '../../../../Guarantee';
+import { ProPlusLuminousDynamicMessage } from '../../../../ProPlusLuminousDynamicMessage';
+import { MakeupPromo20231116 } from './Promo';
+
+export const Makeup20231116: FC = () => (
+  <>
+    <MakeupPromo20231116 />
+    <Form
+      courseGroups={courseGroups}
+      school="QC Makeup Academy"
+      guarantee={() => <Guarantee />}
+      agreementLink="https://www.qcmakeupacademy.com/enrollment-agreement.html"
+      agreementLinkGB="https://www.qcmakeupacademy.com/enrollment-agreement-gb.html"
+      successLink="https://www.qcmakeupacademy.com/welcome-to-the-school/"
+      dynamicCourseDescriptions="REPLACE"
+      promoCodeDefault="PROLUMINOUS"
+      dynamicCourseMessages={[ () => <ProPlusLuminousDynamicMessage />, () => <BogoDynamicMessage /> ]}
+      visualPaymentPlans={true}
+      paymentOptionsReverse={true}
+    />
+  </>
+);
