eadd58d7853a1341e423d2a92b5dffa101095e4a
Dave Welsh <dave@qccareerschool.com>
2023-11-16 08:55:12 -0500

    Added SAVE60

--- src/pages/event/continued-education/index.tsx
+++ src/pages/event/continued-education/index.tsx
@@ -1,21 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { Form } from '../../../components/Form';
+import { useDateContext } from '../../../hooks/useDateContext';
+import { dateOverride } from '../../../lib/dateOverride';
 import { courseGroups } from '../courseGroups';
 import { Guarantee } from '../Guarantee';
 import { ContinuedEducationPromo } from './Promo';
 
-export const EventContinuedEducation: FC = () => (
-  <>
-    <ContinuedEducationPromo />
-    <Form
-      courseGroups={courseGroups}
-      school="QC Event School"
-      guarantee={() => <Guarantee />}
-      agreementLink="https://www.qceventplanning.com/enrollment-agreement.html"
-      agreementLinkGB="https://www.qceventplanning.com/enrollment-agreement-gb.html"
-      successLink="https://www.qceventplanning.com/welcome-to-the-school/"
-      student={true}
-    />
-  </>
-);
+const startDate = new Date(Date.UTC(2023, 10, 16, 14, 30)); // November 16 at 09:30 (15:30 UTC)
+const endDate = new Date(Date.UTC(2023, 11, 1, 5)); // December 1 at 00:00 (05:00 UTC)
+
+export const EventContinuedEducation: FC = () => {
+  const serverDate = useDateContext();
+  const date = dateOverride() ?? serverDate;
+
+  const promoCodeDefault = useMemo(() => (date >= startDate && date < endDate ? 'SAVE60' : undefined), [ date ]);
+
+  return (
+    <>
+      <ContinuedEducationPromo />
+      <Form
+        courseGroups={courseGroups}
+        school="QC Event School"
+        guarantee={() => <Guarantee />}
+        agreementLink="https://www.qceventplanning.com/enrollment-agreement.html"
+        agreementLinkGB="https://www.qceventplanning.com/enrollment-agreement-gb.html"
+        successLink="https://www.qceventplanning.com/welcome-to-the-school/"
+        student
+        promoCodeDefault={promoCodeDefault}
+      />
+    </>
+  );
+};
--- src/pages/makeup/continued-education/index.tsx
+++ src/pages/makeup/continued-education/index.tsx
@@ -1,21 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { Form } from '../../../components/Form';
+import { useDateContext } from '../../../hooks/useDateContext';
+import { dateOverride } from '../../../lib/dateOverride';
 import { courseGroups } from '../courseGroups';
 import { Guarantee } from '../Guarantee';
 import { ContinuedEducationPromo } from './Promo';
 
-export const MakeupContinuedEducation: FC = () => (
-  <>
-    <ContinuedEducationPromo />
-    <Form
-      courseGroups={courseGroups}
-      school="QC Makeup Academy"
-      guarantee={() => <Guarantee />}
-      agreementLink="https://www.qcmakeupacademy.com/enrollment-agreement.html"
-      agreementLinkGB="https://www.qcmakeupacademy.com/enrollment-agreement-gb.html"
-      successLink="https://www.qcmakeupacademy.com/welcome-to-the-school/"
-      student={true}
-    />
-  </>
-);
+const startDate = new Date(Date.UTC(2023, 10, 16, 14, 30)); // November 16 at 09:30 (15:30 UTC)
+const endDate = new Date(Date.UTC(2023, 11, 1, 5)); // December 1 at 00:00 (05:00 UTC)
+
+export const MakeupContinuedEducation: FC = () => {
+  const serverDate = useDateContext();
+  const date = dateOverride() ?? serverDate;
+
+  const promoCodeDefault = useMemo(() => (date >= startDate && date < endDate ? 'SAVE60' : undefined), [ date ]);
+
+  return (
+    <>
+      <ContinuedEducationPromo />
+      <Form
+        courseGroups={courseGroups}
+        school="QC Makeup Academy"
+        guarantee={() => <Guarantee />}
+        agreementLink="https://www.qcmakeupacademy.com/enrollment-agreement.html"
+        agreementLinkGB="https://www.qcmakeupacademy.com/enrollment-agreement-gb.html"
+        successLink="https://www.qcmakeupacademy.com/welcome-to-the-school/"
+        student
+        promoCodeDefault={promoCodeDefault}
+      />
+    </>
+  );
+};
