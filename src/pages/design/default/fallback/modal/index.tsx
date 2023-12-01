c95d65cc9572b984969768ec4db3c05fb1086365
Dave Welsh <dave@qccareerschool.com>
2023-12-01 09:17:53 -0500

    Updated modal text

--- src/pages/design/default/fallback/modal/index.tsx
+++ src/pages/design/default/fallback/modal/index.tsx
@@ -8,13 +8,13 @@ type Props = {
 
 export const DesignFallbackModal: React.FC<Props> = props => (
   <Modal isOpen={props.isOpen} toggle={props.onToggle}>
-    <ModalHeader toggle={props.onToggle}>Career Essentials Collection</ModalHeader>
+    <ModalHeader toggle={props.onToggle}>Course Bonuses Included</ModalHeader>
     <ModalBody>
-      <p className="lead">Start Your Design Career</p>
-      <p>For a limited time, enroll in a design course and get business templates and <i>Design Files</i> software!</p>
+      <p className="lead">Career Essentials Collection</p>
+      <p>Enroll in any design course to receive free design software as well as our brand new Career Essentials Collection filled with business &amp; social media templates!</p>
       <figure>
         <img src={require('./design-files.jpg')} alt="Design Files software" className="img-fluid mb-1" />
-        <figcaption className="h6">4 Months Of Design Files Software FREE</figcaption>
+        <figcaption className="h6">4 Months of <i>Design Files</i> Software FREE</figcaption>
       </figure>
       <figure className="mb-0">
         <img src={require('./downloads.jpg')} alt="template downloads" className="img-fluid mb-1" />
